7b89c6c52e5c4ddc58e0b21ca669007b0383f565
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-10-12 16:30:06 -0800

    WIP searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -13,46 +13,6 @@ const SearchBarExamples: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        
-
-        {/* <p>Searchbar with cancel button always shown</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="always"></IonSearchbar>
-
-        <p>Searchbar with cancel button never shown</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="never"></IonSearchbar>
-
-        <p>Searchbar with cancel button shown on focus</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus"></IonSearchbar>
-
-        <p>Searchbar with danger color</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} color="danger"></IonSearchbar>
-
-        <p>Searchbar with telephone type</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} type="tel"></IonSearchbar>
-
-        <p>Searchbar with numeric inputmode</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} inputmode="numeric"></IonSearchbar>
-
-        <p>Searchbar disabled </p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} disabled={true}></IonSearchbar>
-
-        <p>Searchbar with a cancel button and custom cancel button text</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus" cancelButtonText="Custom Cancel"></IonSearchbar>
-
-        <p>Searchbar with a custom debounce - Note: debounce only works on onIonChange event</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} debounce={1000}></IonSearchbar>
-
-        <p>Animated Searchbar</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} animated></IonSearchbar>
-
-        <p>Searchbar with a placeholder</p>
-        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} placeholder="Filter Schedules"></IonSearchbar>
-
-        <p>Searchbar in a Toolbar</p>
-        <IonToolbar>
-          <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
-        </IonToolbar> */}
-
       </IonContent>
     </IonPage>
   );
